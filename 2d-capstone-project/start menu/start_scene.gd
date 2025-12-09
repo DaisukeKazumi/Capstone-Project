04510f7c75dcb900a3b4a523f05c401da4a31fb7
andreacisc <andreavelez6555@gmail.com>
2025-12-09 14:30:15 -0500

    worked on start scene buttons and added transition between start menu and levels

--- 2d-capstone-project/start menu/start_scene.gd
+++ 2d-capstone-project/start menu/start_scene.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$CanvasLayer/ColorRect.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,8 +12,15 @@ func _process(delta: float) -> void:
 
 
 func _on_play_pressed() -> void:
+	$CanvasLayer/ColorRect.visible = true
+	$CanvasLayer/AnimationPlayer.play("fade_to_black")
+	await $CanvasLayer/AnimationPlayer.animation_finished
 	get_tree().change_scene_to_file('res://Level 2 Assets/Level2.tscn')
 
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_about_pressed() -> void:
+	get_tree().change_scene_to_file('res://start menu/about_scene.tscn')
